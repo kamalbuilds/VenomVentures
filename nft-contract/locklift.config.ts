b49508bfc85d1e2c86cf6627896db6a62bc69512
Kamal Nayan <geniusamansingh@gmail.com>
2023-06-07 06:54:43 +0000

    locklift config success

--- nft-contract/locklift.config.ts
+++ nft-contract/locklift.config.ts
@@ -7,9 +7,12 @@ declare global {
 }
 
 const secret_key = process.env.secretkey || "";
+const phrase = process.env.phrase || "";
 const LOCAL_NETWORK_ENDPOINT = process.env.NETWORK_ENDPOINT || "http://localhost/graphql";
-const DEV_NET_NETWORK_ENDPOINT = process.env.DEV_NET_NETWORK_ENDPOINT || "https://devnet-sandbox.evercloud.dev/graphql";
-
+const DEV_NET_NETWORK_ENDPOINT = process.env.DEV_NET_NETWORK_ENDPOINT || "https://gql-devnet.venom.network/graphql";
+const VENOM_DEVNET_GIVER_ADDRESS = process.env.VENOM_DEVNET_GIVER_ADDRESS || '0:0000000000000000000000000000000000000000000000000000000000000000';
+const VENOM_DEVNET_GIVER_PHRASE = process.env.VENOM_DEVNET_GIVER_PHRASE || '';
+const VENOM_DEVNET_GIVER_KEY = process.env.VENOM_DEVNET_GIVER_KEY || '';
 const VENOM_TESTNET_ENDPOINT = process.env.VENOM_TESTNET_ENDPOINT || "https://jrpc-devnet.venom.foundation/";
 const VENOM_TESTNET_TRACE_ENDPOINT =
   process.env.VENOM_TESTNET_TRACE_ENDPOINT || "https://gql-devnet.venom.network/graphql";
@@ -56,6 +59,7 @@ const config: LockliftConfig = {
       // This giver is default local-node giverV2
       giver: {
         // Check if you need provide custom giver
+	      giverFactory: (ever, keyPair, address) => new SimpleGiver(ever, keyPair, address),
         address: "0:ece57bcc6c530283becbbd8a3b24d3c5987cdddc3c8b7b33be6e4a6312490415",
         key: "172af540e43a524763dd53b26a066d472a97c4de37d5498170564510608250c3",
       },
@@ -71,18 +75,18 @@ const config: LockliftConfig = {
     },
     test: {
       connection: {
-        id: 1,
-        type: "graphql",
+        id: 1002,
+        type: "jrpc",
         group: "dev",
         data: {
-          endpoints: [DEV_NET_NETWORK_ENDPOINT],
-          latencyDetectionInterval: 1000,
-          local: false,
+          endpoint: DEV_NET_NETWORK_ENDPOINT,
         },
       },
       giver: {
-        address: "0:9fa98bf9e91cb0ad1160fb2f20327e7bb9317e48f7d153898d75ccb9940abce4",
-        key: "secret key",
+        giverFactory: (ever, keyPair, address) => new SimpleGiver(ever, keyPair, address),
+        address: VENOM_DEVNET_GIVER_ADDRESS,
+        phrase: VENOM_DEVNET_GIVER_PHRASE,
+        accountId: 0,
       },
       tracing: {
         endpoint: DEV_NET_NETWORK_ENDPOINT,
@@ -90,7 +94,7 @@ const config: LockliftConfig = {
       keys: {
         // Use everdev to generate your phrase
         // !!! Never commit it in your repos !!!
-        // phrase: "action inject penalty envelope rabbit element slim tornado dinner pizza off blood",
+        phrase: VENOM_DEVNET_GIVER_PHRASE,
         amount: 20,
       },
     },
@@ -104,6 +108,7 @@ const config: LockliftConfig = {
         },
       },
       giver: {
+        giverFactory: (ever, keyPair, address) => new SimpleGiver(ever, keyPair, address),
         address: "0:2f58089d5457330607efc2b966d4cc7282d0d726df18e13ae0aa5680cdf3e7bb",
         phrase: "naive talent fork license vacuum rocket slot auction ability birth police wheat",
         accountId: 0,
@@ -132,8 +137,9 @@ const config: LockliftConfig = {
       },
       // This giver is default Wallet
       giver: {
-        address: "0:0000000000000000000000000000000000000000000000000000000000000000",
-        key: secret_key,
+	      giverFactory: (ever, keyPair, address) => new GiverWallet(ever, keyPair, address),
+        address: VENOM_DEVNET_GIVER_ADDRESS,
+        key: VENOM_DEVNET_GIVER_KEY,
       },
       tracing: {
         endpoint: MAIN_NET_NETWORK_ENDPOINT,
@@ -141,7 +147,7 @@ const config: LockliftConfig = {
       keys: {
         // Use everdev to generate your phrase
         // !!! Never commit it in your repos !!!
-        // phrase: "action inject penalty envelope rabbit element slim tornado dinner pizza off blood",
+        phrase: "action inject penalty envelope rabbit element slim tornado dinner pizza off blood",
         amount: 20,
       },
     },
