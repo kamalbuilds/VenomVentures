97f3a624c3f39125e0adacacb6f8cdf210aca880
Kamal Nayan <geniusamansingh@gmail.com>
2023-06-10 05:44:56 +0000

    creating nfts

--- client/src/pages/CreateCampaign.tsx
+++ client/src/pages/CreateCampaign.tsx
@@ -6,6 +6,12 @@ import DateForm from "../components/DateForm";
 import Form, { FormProps, FORM_ERROR } from "../components/Form";
 import LabeledTextField from "../components/FormField";
 import LabeledTextAreaField from "../components/TextAreaForm";
+import { NFTStorage, File } from 'nft.storage';
+import {
+  Address,
+  ProviderRpcClient,
+  Subscriber,
+} from "everscale-inpage-provider";
 
 export function CampaignForm<S extends z.ZodType<any, any>>(
   props: FormProps<S>
@@ -44,7 +50,7 @@ export function CampaignForm<S extends z.ZodType<any, any>>(
               <LabeledTextField
                 name="target"
                 label="Goal"
-                placeholder="ETH 0.005 "
+                placeholder="VENOM 0.005 "
                 type="number"
                 required
                 precision={10}
@@ -93,6 +99,89 @@ const CreateCampaign = () => {
   // call the createCampaign function here
   const navigate = useNavigate();
 
+  
+  const mintnft = async (data) => {
+    try {
+        // const BaseURL = "https://venomventures.vercel.app/api";
+      const BaseURL = "http://localhost:3000/api";
+      const nftstorage = new NFTStorage({ token: process.env.NEXT_PUBLIC_NFTSTORAGE_API_KEY })
+      const ipfs_image =
+        typeof data.image == "string"
+          ? data.image
+          : await nftstorage.store(data.image);
+
+      const nftjson = JSON.stringify({
+        type: "Basic NFT",
+        id: 0,
+        name: data.name,
+        description: data.description,
+        preview: {
+          source: ipfs_image.replace(
+            "ipfs://",
+            "https://gateway.ipfscdn.io/ipfs/"
+          ),
+          mimetype: "image/png",
+        },
+        files: [
+          {
+            source: ipfs_image.replace(
+              "ipfs://",
+              "https://gateway.ipfscdn.io/ipfs/"
+            ),
+            mimetype: ipfs_image.replace(
+              "ipfs://",
+              "https://gateway.ipfscdn.io/ipfs/"
+            ),
+          },
+        ],
+        attributes: data.properties.filter((e) => e.type.length > 0),
+        external_url: "https://venomventures.vercel.app/",
+        nft_image: ipfs_image,
+        collection_name: data.collection,
+      });
+
+      const contract = new venomProvider.Contract(
+        collectionAbi,
+        collection_address
+      );
+      const { count: id } = await contract.methods
+        .totalSupply({ answerId: 0 })
+        .call();
+      console.log({ id });
+
+      const subscriber = new Subscriber(venomProvider);
+      contract
+        .events(subscriber)
+        .filter((event) => event.event === "tokenCreated")
+        .on(async (event) => {
+          const { nft: nftAddress } = await contract.methods
+            .nftAddress({ answerId: 0, id: id })
+            .call();
+
+          const res = await axios({
+            url: `${BaseURL}/createnft`,
+            method: "POST",
+            data: {
+              nft_address: nftAddress._address,
+              tokenId: event.data.tokenId,
+              collection_name: data.collection,
+              json: nftjson,
+              owner: signer_address,
+            },
+          });
+
+          console.log(res.data);
+        });
+      const outputs = await contract.methods.mintNft({ json: nftjson }).send({
+        from: new Address(signer_address),
+        amount: "1000000000",
+      });
+
+    } catch (error) {
+      alert(error.message);
+      console.log(error.message);
+    }
+  };
 
   return (
     <div>
@@ -106,6 +195,8 @@ const CreateCampaign = () => {
         onSubmit={async (values) => {
           try {
             // mint nft
+            console.log(values,"vsdf");
+            mintnft(values);
           } catch (error: any) {
             console.error(error);
             showNotification({
---
+++ client/src/pages/api/createnft.js
@@ -0,0 +1,65 @@
+import dbConnect from "@/lib/dbConnect";
+import Nft from "../../models/Nft";
+import User from "../../models/User";
+import NFTCollection from "../../models/NFTCollection";
+
+export default async function handler(req, res) {
+  const { method } = req;
+
+  await dbConnect();
+
+  switch (method) {
+    case "GET":
+      try {
+        const nfts = await Nft.find({}).populate("owner");
+        res.status(200).json({ success: true, data: nfts });
+      } catch (error) {
+        res.status(400).json({ success: false, data: error.message });
+      }
+      break;
+    case "POST":
+      try {
+        let nft;
+        const { tokenId, collection_name, json, owner, nft_address } = req.body;
+
+        // check if nft exists then return
+        nft = await Nft.findOne({ tokenId: req.body.tokenId });
+        if (nft) return res.status(201).json({ success: true, data: nft });
+
+        //check if given collection exists
+        const nft_collection = await NFTCollection.findOne({
+          name: collection_name,
+        });
+
+        if (!nft_collection)
+          return res
+            .status(400)
+            .json({ success: false, data: "Cannot find the collection" });
+
+        //check if user exists
+        let nft_owner = await User.findOne({ wallet_id: owner });
+        if (!nft_owner)
+          return res
+            .status(400)
+            .json({ success: false, data: "Cannot Find The Owner" });
+
+        nft = await Nft.create({
+          nft_address,
+          tokenId,
+          nft_collection,
+          collection_name,
+          json,
+          owner: nft_owner,
+        });
+
+        res.status(201).json({ success: true, data: nft });
+      } catch (error) {
+        res.status(400).json({ success: false, data: error.message });
+      }
+      break;
+
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+}
